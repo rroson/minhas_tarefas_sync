dfb2650002f786810a30b362eb96c734f132be2a
Ricardo Roson <rroson@gmail.com>
2024-07-08 16:09:12 -0300

    Tentando let HTML

--- minhas_tarefas/app.py
+++ minhas_tarefas/app.py
@@ -1,7 +1,22 @@
+from http import HTTPStatus
 from fastapi import FastAPI
+from minhas_tarefas.schemas import Mensagem
+from fastapi.responses import HTMLResponse
+from fastapi.requests import Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
-@app.get("/")
+@app.get("/", status_code=HTTPStatus.OK, response_model=Mensagem)
 def read_root():
-    return {'Message': 'Olá mundo!'}
+    return {'mensagem': 'Olá mundo!'}
+
+@app.get("/html", response_class=HTMLResponse)
+def retorna_html():
+    async def read_item(request: Request):
+        return templates.TemplateResponse(
+            request=request, name="index.html"
+        )
