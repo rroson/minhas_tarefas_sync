c6258c3034826ecca8881bd0885d1dd6cbca7ffb
Ricardo Roson <rroson@gmail.com>
2024-07-10 11:29:35 -0300

    CRUD parte 1

--- minhas_tarefas/app.py
+++ minhas_tarefas/app.py
@@ -1,6 +1,6 @@
 from http import HTTPStatus
 from fastapi import FastAPI
-from minhas_tarefas.schemas import Mensagem
+from minhas_tarefas.schemas import Mensagem, UsuarioSchema, UsuarioPublic, UsuarioDB
 from fastapi.responses import HTMLResponse
 from fastapi.requests import Request
 from fastapi.staticfiles import StaticFiles
@@ -9,11 +9,21 @@ from fastapi.templating import Jinja2Templates
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
+database = [] #Fake Database
 
 @app.get("/", status_code=HTTPStatus.OK, response_model=Mensagem)
 def read_root():
     return {'mensagem': 'Olá mundo!'}
 
-@app.get("/html", response_class=HTMLResponse)
+@app.get("/html/", response_class=HTMLResponse)
 async def retorna_html(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UsuarioPublic)
+def create_user(usuario: UsuarioSchema):
+    usuario_id = UsuarioDB(
+        id=len(database) + 1,
+        **usuario.model_dump() # Altera o objeto UsuarioSchema para Dicionário
+    )
+    database.append(usuario_id)
+    return usuario_id
--- minhas_tarefas/schemas.py
+++ minhas_tarefas/schemas.py
@@ -1,6 +1,18 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
 
 class Mensagem(BaseModel):
     mensagem: str
 
+class UsuarioSchema(BaseModel):
+    username: str
+    email: EmailStr
+    password: str
+
+class UsuarioDB(UsuarioSchema):
+    id: int
+
+class UsuarioPublic(BaseModel):
+    id: int
+    username: str
+    email: EmailStr
