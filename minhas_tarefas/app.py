4f6113f5e13b38ea893368e34ff15e9497524044
Ricardo Roson <rroson@gmail.com>
2024-07-11 15:08:46 -0300

    CRUD Final

--- minhas_tarefas/app.py
+++ minhas_tarefas/app.py
@@ -1,5 +1,5 @@
 from http import HTTPStatus
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from minhas_tarefas.schemas import Mensagem, UsuarioSchema, UsuarioPublic, UsuarioDB, UsuarioList
 from fastapi.responses import HTMLResponse
 from fastapi.requests import Request
@@ -11,13 +11,13 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 database = [] #Fake Database
 
-@app.get("/", status_code=HTTPStatus.OK, response_model=Mensagem)
-def read_root():
-    return {'mensagem': 'Olá mundo!'}
-
 @app.get("/html/", response_class=HTMLResponse)
 async def retorna_html(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/", status_code=HTTPStatus.OK, response_model=Mensagem)
+def read_root():
+    return {'mensagem': 'Olá mundo!'}
 
 @app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UsuarioPublic)
 def create_user(usuario: UsuarioSchema):
@@ -31,3 +31,19 @@ def create_user(usuario: UsuarioSchema):
 @app.get('/users/', response_model=UsuarioList)
 def read_users():
     return {'users': database}
+
+@app.put('/users/{user_id}', response_model=UsuarioPublic)
+def read_user(user_id: int, users: UsuarioSchema):
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(status_code = HTTPStatus.NOT_FOUND, detail = 'Usuário não encontrado')
+    user_with_id = UsuarioDB(**users.model_dump(), id = user_id)
+    database[user_id - 1] = user_with_id
+
+    return user_with_id
+
+@app.delete('/users/{user_id}', response_model=Mensagem)
+def delete_user(user_id: int):
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(status_code = HTTPStatus.NOT_FOUND, detail = 'Usuário não encontrado')
+    del database[user_id - 1]
+    return {'mensagem': 'Usuário excluído com sucesso'}
--- tests/test_app.py
+++ tests/test_app.py
@@ -25,3 +25,38 @@ def test_create_user(client):
         'email': 'alice@example.com',
         'id': 1,
     }
+
+def test_read_users(client):
+    respones = client.get('/users/')
+    assert respones.status_code == HTTPStatus.OK
+    assert respones.json() == {
+        'users': [
+            {
+                'username': 'alice',
+                'email': 'alice@example.com',
+                'id': 1,
+            }
+        ]
+    }
+
+def test_update_user(client):
+    response = client.put(
+        '/users/1',
+        json={
+            'username': 'bob',
+            'email': 'bob@example.com',
+            'password': 'mynewpassword',
+        },
+    )
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'username': 'bob',
+        'email': 'bob@example.com',
+        'id': 1,
+    }
+
+def test_delete_user(client):
+    response = client.delete('/users/1')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'mensagem': 'Usuário excluído com sucesso'}
